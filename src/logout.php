49f1aadb2626285883cd8ed072434a30fa5c7a67
mmattis <mattis.milochau@ynov.com>
2024-04-14 15:30:15 +0200

    files orga

--- src/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Détruire toutes les données de session
-session_destroy();
-
-// Rediriger vers la page de connexion ou une autre page de votre choix
-header("Location: index.php");
-exit();
